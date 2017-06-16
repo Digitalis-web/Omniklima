61872a5bfe2424ad334a8b6bf50d03618a6da57f
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-06-16 13:30:56 +0200

    Js effects

--- app/index.php
+++ app/index.php
@@ -17,9 +17,9 @@
             <div id="video" class="video_player"></div>
 
             <div class="video_text_container col-xs-12">
-                <h1>RCA road condition ahead </h1>
-                <h2>Technique delivered by OmniKlima</h2>
-                <button>play video in fullscreen</button>
+                <h1 class="fade-delay-1 fade-in">RCA road condition ahead </h1>
+                <h2 class="fade-delay-2 fade-in">Technique delivered by OmniKlima</h2>
+                <button class="fade-delay-2 fade-in">play video in fullscreen</button>
             </div>
         </div>
     </div>
@@ -30,12 +30,12 @@
     <div class="row-fluid info">
         <div class="col-xs-12 info_container">
             <div class="info_img_container col-md-3 col-md-offset-0 col-xs-10 col-xs-offset-1">
-                <img src="img/logo/Logo.svg" alt="Omniklima logo">
+                <img class="slide-in-left slide-in" src="img/logo/Logo.svg" alt="Omniklima logo">
             </div>
             <div class="info_text_container col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
-                <h1>Solutions for road condition</h1>
-                <p>Driving conditions ahead of a driving vehicle.</p>
-                <p>A technique delivered by OmniKlima.</p>
+                <h1 class="fade-delay-1 fade-in">Solutions for road condition</h1>
+                <p class="fade-delay-2 fade-in">Driving conditions ahead of a driving vehicle.</p>
+                <p class="fade-delay-2 fade-in">A technique delivered by OmniKlima.</p>
             </div>
         </div>
     </div>
@@ -44,7 +44,7 @@
 <section class="contact container-fluid">
     <div class="row-fluid contact">
         <div class="contact_container col-xs-12">
-            <div class="col-xs-6 col-xs-offset-3 contact_text_container">
+            <div class="col-xs-6 col-xs-offset-3 contact_text_container fade-in">
                 <h1>contact</h1>
                 <h2>Torbjörn gustavsson</h2>
                 <p>torbjorn.gustavsson@klimator.se</p>
