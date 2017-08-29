642da46caa8a8c412e6ec8966b52fe6c19458c4c
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-08-29 18:21:17 +0200

    smal uppdate

--- app/index.php
+++ app/index.php
@@ -2,13 +2,43 @@
 <?php include "partials/head.php" ?>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1"/>
+
     <title>OmniKlima</title>
+    <meta name="author" content="Digital penguin solutions">
     <meta name="keywords" content="Road condition, OmniKlima, climate"/>
     <meta name="description" content="Driving conditions ahead of a driving vehicle."/>
+
     <!--js files-->
     <script src="js/app.js"></script>
     <script src="js/third_party.js"></script>
     <script async src="https://www.youtube.com/iframe_api"></script>
+
+    <!--css-->
+    <link href="css/app.css" rel="stylesheet">
+
+    <!--JQuery-->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+
+    <!--icon-->
+    <link rel="icon" href="img/logo/favicon.png">
+    <!--<script src = "js/app.js" type="text/javascript"></script>-->
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"
+            integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY"
+            crossorigin="anonymous"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"
+            integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo"
+            crossorigin="anonymous"></script>
+    <![endif]-->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"
+            integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb"
+            crossorigin="anonymous"></script>
 </head>
 
 <body class="wrapper col-xs-12">
@@ -58,8 +88,6 @@
         </div>
     </div>
 </section>
-
-
 
 </body>
 </html>
